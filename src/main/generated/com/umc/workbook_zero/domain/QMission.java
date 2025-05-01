44e2d30b48392af6a60a0aaa944767187fd2a874
김규리 <kimkyuree1@naver.com>
2025-05-01 14:05:13 +0900

    Feat: Q클래스 생성

---
+++ src/main/generated/com/umc/workbook_zero/domain/QMission.java
@@ -0,0 +1,67 @@
+package com.umc.workbook_zero.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMission is a Querydsl query type for Mission
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMission extends EntityPathBase<Mission> {
+
+    private static final long serialVersionUID = 253962515L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMission mission = new QMission("mission");
+
+    public final com.umc.workbook_zero.domain.common.QBaseEntity _super = new com.umc.workbook_zero.domain.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final DatePath<java.time.LocalDate> deadline = createDate("deadline", java.time.LocalDate.class);
+
+    public final ListPath<com.umc.workbook_zero.domain.mapping.MemberMission, com.umc.workbook_zero.domain.mapping.QMemberMission> memberMissionList = this.<com.umc.workbook_zero.domain.mapping.MemberMission, com.umc.workbook_zero.domain.mapping.QMemberMission>createList("memberMissionList", com.umc.workbook_zero.domain.mapping.MemberMission.class, com.umc.workbook_zero.domain.mapping.QMemberMission.class, PathInits.DIRECT2);
+
+    public final NumberPath<Long> missionId = createNumber("missionId", Long.class);
+
+    public final StringPath missionSpec = createString("missionSpec");
+
+    public final NumberPath<Integer> reward = createNumber("reward", Integer.class);
+
+    public final QStore store;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QMission(String variable) {
+        this(Mission.class, forVariable(variable), INITS);
+    }
+
+    public QMission(Path<? extends Mission> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMission(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMission(PathMetadata metadata, PathInits inits) {
+        this(Mission.class, metadata, inits);
+    }
+
+    public QMission(Class<? extends Mission> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.store = inits.isInitialized("store") ? new QStore(forProperty("store"), inits.get("store")) : null;
+    }
+
+}
+
